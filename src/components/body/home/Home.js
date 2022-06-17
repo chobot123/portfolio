78b27c83529a72660abd51a732b86f084e945db7
chobot123 <chojoshua99@gmail.com>
2022-06-16 22:08:47 -0700

    Created template for bb8

--- src/components/body/home/Home.js
+++ src/components/body/home/Home.js
@@ -19,19 +19,49 @@ function Home(){
 
 
     return(
-        <Container fluid className="home d-flex flex-column justify-content-center align-items-center mb-1"
+        <Container fluid className="home d-flex flex-column justify-content-center align-items-start"
             style={
                 {
                     height: header,
                 }
             }
         > 
-            <div className="message-container mb-4">
+            <div className="message-container mb-5 pb-5">
                 <div className="display-2" id="myName">Hi, I'm Josh.</div>
                 <div className="h1">I'm a full stack web developer.</div>
                 <button className="h2 view-work">Check Me Out &#8615;</button>
             </div>
-            
+            <div className="bot">
+                <div className="rolling-droid">
+                    <div className="head">
+                        <div className="neck"></div>
+                        <div className="head-marking"></div>
+                        <div className="antenna"></div>
+                        <div className="antenna-two"></div>
+                        <div className="eyes"></div>
+                        <div className="markings">
+                            <div className="marking"></div>
+                            <div className="marking"></div>
+                            <div className="marking"></div>
+                        </div>
+                        <div className="camera">
+                            <div className="camera-button"></div>
+                        </div>
+                    </div>
+                    <div className="body">
+                        <div className="core">
+                            <div className="inner-core">
+                                <div className="bracket top"></div>
+                                <div className="bracket left"></div>
+                                <div className="bracket bot"></div>
+                                <div className="bracket right"></div>
+                                <div className="battery"></div>
+                            </div>
+                            <div className="left-core"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Container>
     )
 }
